736a2badbf5f6c9e5cda30230b8afe4ce4300fe1
shoieb-alam <86704871+shoieb-alam@users.noreply.github.com>
2021-09-26 00:40:23 +0600

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = () => {
+    return (
+        <div>
+            <h4>Hello from Cart</h4>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h4>Hello from Header</h4>
+        </div>
+    );
+};
+
+export default Header;
